3ee4f41efc5573eaadfa722e8b476cda9abcc11b
S Jones <siouxgeonz@gmail.com>
2019-12-14 18:05:27 +0000

    Fixed layout of second part by making display "block" and made start button bigger. (Didn't figure out how to give them their own column.)

--- script.js
+++ script.js
@@ -1,123 +1,109 @@
  
 var factNum=5;// the starting number to be multiplied by; the 'answer'
   // 
+  // the order in which the facts will be presented. 0, 1 and ten are "rule based" and should be done before we do this.
 var factSequence=[2,5,4,9,3,6,7,8];
+// the initial setting - arrays begin with "0"
 var factIndex=0;
+// sets the factSet to the right number.   It will change as we go. 
 var factSet=factSequence[factIndex];
 
 
+
+// this changes the times tables fact set to the next in the sequence
 function nextFact()
 {
     factIndex++;  
     factSet=factSequence[factIndex];
-  p1.innerHTML="next fact is" + factSet;
-  startUp();
-  // we *will* if this works ... have it go to the next num in the array that is our sequence ***FUTURE THING HERE ***
-  }
-function startOver()
-{
-  factSet=2;
-  startUp();
+    p1.innerHTML="next fact is" + factSet;
+    startUp();
 }
+
+// this displays the first fact in whatever set you are on; so if you want to start the fact you're on over, it will do that. Also works at beginning. 
 function startUp()
 {
-     A1.style.display="none";
+  // we'll hide the second part of the question at first. 
+  A1.style.display="none";
   check2.style.display="none";
+
+  // start with our number x 1. 
   factNum=1;
+  // reset the buttons 
     check1.disabled=false;
   start.disabled=true;
   reset.disabled=false;
   startFact.disabled=false;
-   
-   
-
-     newFact();
+  // present the problem and call up its picture. 
+  newFact();
 }
+
+function startOver()
+{
+  // make sure we're starting with the twos. 
+  factSet=2;
+  startUp();
+}
+
 
 function newFact()
-{if (factNum>10)
+// first make sure we're only going to ten. 
 {
-  p1.innerHTML= "you're done!" +'<button id="check1" class="button1" onclick="nextFact();">Next Fact</button>'
-;
-}
+  if (factNum>10)
+  {
+  p1.innerHTML= "you're done!" +'<button id="check1" class="button1" onclick="nextFact();">Next Fact</button>';
+  } 
 // maybe that should be in "check answer" or in both. 
 else 
   {
-    // p1.innerHTML="fact number  is: " + factNum;
-  factor2.innerHTML= factSet + " x " + '<input type="number" id="factor" size="1" maxlength="2"/>';
-   A1.style.display="none";
-  check2.style.display="none";
-   A1.innerHTML=  factSet + " x " + factNum + " =" + ' <input type="number" id="multAnswer"  size="1" maxlength="2"/>';
- check1.disabled=false;
-}
+        // p1.innerHTML="fact number  is: " + factNum;  
+        // now type the question in for this fact, with the input box.
+      factor2.innerHTML= factSet + " x " + '<input type="number" id="factor" size="1" maxlength="2"/>';
+      // take away the second part if it was there from earlier question, but put in the right text for later.  
+      A1.style.display="none";
+      check2.style.display="none";
+      A1.innerHTML=  factSet + " x " + factNum + " =" + ' <input type="number" id="multAnswer"  size="1" maxlength="2"/>';
+    check1.disabled=false;
+    }
+// here's where we create the number chart and then shade in the multiples of the fact. 
 createMultImage();
 changeMultiples('section > div','palevioletred','yellow');
 }
 
-
-
-
+// build a number chart from 1 to the answer, including the numbers. 
 function createMultImage()
 {
+  // make the string of text and variables that you want to be displayed
   let multPicHTML="";
-   let numMax = factSet*10;
+  let numMax = factSet*10;
   for (let num=1; num<=numMax; num++)
   { 
-     //multPicHTML +=' <div class="item"> ' + num + ' </div>';
-     // oh, let's give them each an ID
-     multPicHTML +='  <div class = "item" id="d'+num + '" >' + num + ' </div>'; 
-       }
- 
-  multPic.innerHTML=multPicHTML;
-    
+   multPicHTML +='  <div class = "item" id="d'+num + '" >' + num + ' </div>'; 
+  }
+  // now make it the HTML
+   multPic.innerHTML=multPicHTML;
 }
- 
- 
-
-// Following code: so hitting enter is like clicking "check answer."
-var input = document.getElementById("factor");
-input.addEventListener("keyup", function(event)
- {
-  if (event.keyCode === 13) {
-   event.preventDefault();
-   document.getElementById("check1").click();
-  }
-})
-var input2 = document.getElementById("multAnswer");
-input2.addEventListener("keyup", function(event)
- {
-  if (event.keyCode === 13) {
-   event.preventDefault();
-   document.getElementById("check2").click();
-  }
-})
-
 
 function checkName(picNum)
 {
 var b = factor.value;
 if (b=="")
-{
-  feedback.innerHTML="please enter an answer";
-}
+  {
+    feedback.innerHTML="please enter an answer";
+  }
 else if (b==factNum)
-{
-  feedback.innerHTML="Awesome!  That's the right second factor!"
- document.getElementById("A1").style.display = 'inline';
- document.getElementById("check2").style.display = 'inline';
-  check1.disabled=true;
-  check2.disabled=false;
-  check2.style.color="white";
- 
-}
+  {
+    feedback.innerHTML="Awesome!  That's the right second factor!"
+    document.getElementById("A1").style.display = 'block';
+    document.getElementById("check2").style.display = 'inline';
+    check1.disabled=true;
+    check2.disabled=false;
+    check2.style.color="white";
+  }
 else
-{
-  feedback.innerHTML="Check your entry; it doesn't seem to be right. I'm just a computer, though..."
+    {
+      feedback.innerHTML="Check your entry; it doesn't seem to be right. I'm just a computer, though..."
+    }
 }
-}
-
-
-
 
 
 function checkAnswer()
--- script2.js
+++ script2.js
@@ -1,13 +1,4 @@
- 
-//changeBackgrounds('section > div','yellow');
-
-// function changeBackgrounds(selectorStr, bgColor) {
-//   document.querySelectorAll(selectorStr).forEach(elem => {
-//     elem.style.backgroundColor = bgColor;
-//   });
-// }
-
-
+ // These are functions that didn't work when they were in the same .js file as the one that created the divs they are shading. 
 function changeMultiples(selectorStr, oldColor, bgColor) {
   document.querySelectorAll(selectorStr).forEach(elem => {
     elem.style.backgroundColor = oldColor;
@@ -23,6 +14,25 @@ function changeMultiples(selectorStr, oldColor, bgColor) {
 }
   
    
-   
+   //  make hitting enter like clicking "check answer."  Except it doesn't work anymore. I tried moving it to the beginning as well as moving it here. It used to work. we'll see what happens when we go online with it. 
+var input = document.getElementById("factor");
+input.addEventListener("keyup", function(event)
+ {
+  if (event.keyCode === 13) {
+   event.preventDefault();
+   document.getElementById("check1").click();
+  }
+})
+
+var input2 = document.getElementById("multAnswer");
+input2.addEventListener("keyup", function(event)
+ {
+  if (event.keyCode === 13) {
+   event.preventDefault();
+   document.getElementById("check2").click();
+  }
+})
+
+
     
  
